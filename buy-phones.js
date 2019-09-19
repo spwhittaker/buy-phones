0b20c46985ddfe46a00d1d33b3032d0cec161211
Stephen Whittaker <stephenwhittaker23@gmail.com>
2019-09-19 18:30:23 +0100

    Fixed formatting, final bank balance is now equal to final bank balance plus total spent

--- buy-phones.js
+++ buy-phones.js
@@ -1,27 +1,36 @@
-function buyCases(bal,phon,cas,vat){
+
+
+function buyPhones(bal,phon,cas,vat){
 
 var bankBalance = bal;
 var phoneCost = phon;
 var caseCost = cas;
 var phonesBought = 0;
 var casesBought = 0;
-const VAT = vat;  //Value-added tax, for non-Brits
-let totalSpent = 0;
-var totalCost = phoneCost + phoneCost * VAT;
-
-while (bankBalance < totalCost) {return "What are you buying phones for, you pauper?"}
-
-while (bankBalance >= totalCost) {
-bankBalance = bankBalance - totalCost;
+var taxPercentage = vat;  //Value-added tax, for non-Brits
+var totalSpent = 0;
+var totalPhoneCost = phoneCost + (phoneCost * taxPercentage/100);
+var totalCaseCost = caseCost + (caseCost * taxPercentage/100);
+  
+if (bankBalance < totalPhoneCost) {console.log("What are you buying phones for, you pauper?");}
+else {
+while (bankBalance >= totalPhoneCost) {
+bankBalance = bankBalance - totalPhoneCost;
+//console.log('Balance is now £' + bankBalance.toFixed(2));
 phonesBought ++;
-totalSpent += totalCost;
-if (bankBalance >= caseCost) {
-bankBalance = bankBalance - caseCost;
+//console.log('Number of phones is now ' + phonesBought);
+totalSpent += totalPhoneCost;
+//console.log('Total spent is now £' + totalSpent);
+if (bankBalance >= totalCaseCost){
+bankBalance = bankBalance - totalCaseCost;
+//console.log('Balance is now £' + bankBalance.toFixed(2));
 casesBought++;
-totalSpent += caseCost;}
-else {return "You can't afford the case as well, buddy";}} //Aren't multiple phones enough?
+//console.log('Number of cases is now ' + phonesBought);
+totalSpent += totalCaseCost;}}
+//console.log('Total spent is now £' + totalSpent);} */
+//else {return "You can't afford the case as well, buddy";}
 
 console.log("Bank Balance: £" + bankBalance.toFixed(2));
 console.log("Phones I now own: " + phonesBought);
 console.log("Cases I now own: " + casesBought);
-console.log("Money I've spent on phones and cases: £" + totalSpent)}
+console.log("Money I've spent on phones and cases: £" + totalSpent.toFixed(2));}}
